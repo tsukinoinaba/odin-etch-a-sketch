d19de0200af9f144b3f21b816003dcaf44e89cc8
Velvet <172956088+tsukinoinaba@users.noreply.github.com>
2024-08-22 18:28:29 +0800

    Add button to generate new grid

--- etch.js
+++ etch.js
@@ -17,7 +17,16 @@ function generateGrid (size) {
 }
 
 function colourOnHover () {
-    this.style.backgroundColor = "lavender";
+    this.style.backgroundColor = "purple";
+}
+
+function createPopup () {
+    const width = Number(prompt("Enter width of new grid"));
+    if (typeof width !== "number" || width < 0 || width % 1 !== 0) {
+        alert("Invalid value for grid width! Please enter a positive whole number.");
+        return;
+    }
+    generateGrid(width);
 }
 
 const container = document.querySelector("#container");
